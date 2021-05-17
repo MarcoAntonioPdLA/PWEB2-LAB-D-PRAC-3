0d0a50742d91531d20f650c38556b9159305b8e8
M-UNSA <82843764+M-UNSA@users.noreply.github.com>
2021-05-17 04:21:49 -0500

    Se agregó HiddenMenu y se modificó getMdFiles

--- pub/js/client.js
+++ pub/js/client.js
@@ -1,4 +1,3 @@
-//Función para generar menú y añadir clase responsive
 function hiddenMenu(){
     var x = document.getElementById("myMenu");
     if (x.className === "menu") {
@@ -6,73 +5,75 @@ function hiddenMenu(){
     } else {
         x.className = "menu";
     }
-} 
+}  
+
 //Con estos 2 métodos se enlistan los archivos
 //Método que hace la consulta al servidor
 function showFilesList() {
-  const url = 'http://localhost:3000/showFilesList';
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector('#result').innerHTML = getMdFiles(data);
-    }
-  );
+    const url = 'http://localhost:3000/showFilesList';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        document.querySelector('#result').innerHTML = getMdFiles(data);
+      }
+    );
 }
 //Método que estructura la forma en que se verán los archivos
 function getMdFiles(data) {
-  let content = '\n<h2>Archivos</h2>\n';
-  content += '<ul>\n';
-  for (let i = 0; i < data.length; i++){
-    content += '  <li onclick = "readFile(this.innerHTML)">' + data[i] + '</li>\n';
-  } 
-  content += "</ul>\n";
+    let content = '\n<h2>Archivos</h2>\n';
+    content += '<ul>\n';
+    for (let i = 0; i < data.length; i++){
+        content += '  <li><a href = "javascript:readFile(this.innerHTML)">' + data[i] + '</a></li>\n';
+    } 
+    content += "</ul>\n";
 
-  console.log(content);
-  return content;
+    console.log(content);
+    return content;
 }
 
 function readFile(file) {
-  console.log(file);
+    console.log(file);
 }
 
-function nuevoArchivo(){//esta funcion muestra el formulario
+function newFile(){ //esta funcion muestra el formulario
 //formulario
-console.log(html);
-document.querySelector('#paginaCrear').innerHTML=html;//cuando hacemos click en el menu  para crear un nuevo archivo nos manda al formulario para llenarlo
-sentInformation();
+    console.log(html);
+    document.querySelector('#paginaCrear').innerHTML=html;//cuando hacemos click en el menu  para crear un nuevo archivo nos manda al formulario para llenarlo
+    sentInformation();
 }
 
 function sentInformation(){//sacar la informacion del formulario qeu enviaron del cliente
     
-  const nombreFile=document.querySelector('#nombreArchivo')//guarda el nombre del archivo
-  const textoFile =document.querySelector('#textoDelMarkdown')//guarda el contenido del textarea
-  document.querySelector('#formulario').onsubmit=()=>{//luego de hacer submit en el formulario nos envia a la funcion guardar
-      console.log(textoFile.value)
-      console.log(nombreFile.value)
-      save(textoFile,value,nombreFile.value)//this funcion
-      return false;
-  }
+    const nombreFile=document.querySelector('#nombreArchivo')//guarda el nombre del archivo
+    const textoFile =document.querySelector('#textoDelMarkdown')//guarda el contenido del textarea
+    document.querySelector('#formulario').onsubmit=()=>{//luego de hacer submit en el formulario nos envia a la funcion guardar
+        console.log(textoFile.value)
+        console.log(nombreFile.value)
+        save(textoFile,value,nombreFile.value)//this funcion
+        return false;
+    }
 }
 function save(text,nombreArchivo){
-  const link='http://localhost:3000/guardarServidor'//la apliacion del servidor se debe llamar;
 
-  const data={
-    texto:text,
-    nomb:nombreArchivo
-      }
-  console.log(data)
-  const request={
-      method:'POST',
-      headers:{
-          'Content-Type':'aplication/json',
-      },
-      body: JSON.stringify(data),
-  }
-  fetch(link,request)
-  explorar();
+    const link='http://localhost:3000/guardarServidor'//la apliacion del servidor se debe llamar;
+
+    const data={
+        texto:text,
+        nomb:nombreArchivo
+    }
+    console.log(data)
+    const request={
+        method:'POST',
+        headers:{
+            'Content-Type':'aplication/json',
+        },
+        body: JSON.stringify(data),
+    }
+    fetch(link,request)
+    explorar();
 }
 
 //Esta función va al final
 document.addEventListener("DOMContentLoaded", function() {
-  showFilesList();
+showFilesList();
 })
